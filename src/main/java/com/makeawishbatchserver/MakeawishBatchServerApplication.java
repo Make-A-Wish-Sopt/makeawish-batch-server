25b92c1a6aea8a3219a3940714936c26e875a56e
yang Jiyoung <0112jyoung@naver.com>
2025-03-04 20:39:38 +0900

    [init] 프로젝트 세팅

---
+++ src/main/java/com/makeawishbatchserver/MakeawishBatchServerApplication.java
@@ -0,0 +1,13 @@
+package com.makeawishbatchserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakeawishBatchServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MakeawishBatchServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/makeawishbatchserver/MakeawishBatchServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.makeawishbatchserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakeawishBatchServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
